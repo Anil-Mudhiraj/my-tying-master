5329b992d31239cd3156a0fcde1789ca52cb2988
anil-mudhiraj <anil9618454029@gmail.com>
2022-09-23 18:17:57 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,210 @@
+import {useState, useEffect, useRef} from 'react'
+import randomWords from 'random-words'
+
+const NUMB_OF_WORDS = 200
+const SECONDS = 60
+
+
+function App() {
+  const [words, setWords] = useState([])
+  const [countDown, setCountDown] = useState(SECONDS)
+  const [seconds,setSeconds] = useState(SECONDS)
+  const [currInput, setCurrInput] = useState("")
+  const [currWordIndex, setCurrWordIndex] = useState(0)
+  const [currCharIndex, setCurrCharIndex] = useState(-1)
+  const [currChar, setCurrChar] = useState("")
+  const [correct, setCorrect] = useState(0)
+  const [incorrect, setIncorrect] = useState(0)
+  const [status, setStatus] = useState("waiting")
+  const textInput = useRef(null)
+
+  // useEffect(() => {
+  //   setWords(generateWords())
+  // }, [])
+
+  useEffect(() => {
+    if (status === 'started') {
+      textInput.current.focus()
+    }
+  }, [status])
+
+  function generateWords() {
+    // let text = "A string or regular expression to use for splitting If omitted, an array with the original string is returned."
+    // return new Array(NUMB_OF_WORDS).fill(null).map(() => randomWords())
+   
+  }
+
+  function start() {
+
+    if (status === 'finished') {
+      setWords(generateWords())
+      setCurrWordIndex(0)
+      setCorrect(0)
+      setIncorrect(0)
+      setCurrCharIndex(-1)
+      setCurrChar("")
+    }
+
+    if (status !== 'started') {
+      setStatus('started')
+      let interval = setInterval(() => {
+        setCountDown((prevCountdown) => {
+          if (prevCountdown === 0) {
+            clearInterval(interval)
+            setStatus('finished')
+            setCurrInput("")
+            return SECONDS
+          } else {
+            return prevCountdown - 1
+          }
+        }  )
+      } ,  1000 )
+    }
+    
+  }
+
+  function handleKeyDown({keyCode, key}) {
+    // space bar 
+    if (keyCode === 32) {
+      checkMatch()
+      setCurrInput("")
+      setCurrWordIndex(currWordIndex + 1)
+      setCurrCharIndex(-1)
+    // backspace
+    } else if (keyCode === 8) {
+      setCurrCharIndex(currCharIndex - 1)
+      setCurrChar("")
+    } else {
+      setCurrCharIndex(currCharIndex + 1)
+      setCurrChar(key)
+    }
+  }
+
+  function checkMatch() {
+    const wordToCompare = words[currWordIndex]
+    const doesItMatch = wordToCompare === currInput.trim()
+    if (doesItMatch) {
+      setCorrect(correct + 1)
+    } else {
+      setIncorrect(incorrect + 1)
+    }
+  }
+
+  function getCharClass(wordIdx, charIdx, char) {
+    if (wordIdx === currWordIndex && charIdx === currCharIndex && currChar && status !== 'finished') {
+      if (char === currChar) {
+        return 'has-background-success'
+      } else {
+        return 'has-background-danger'
+      }
+    } else if (wordIdx === currWordIndex && currCharIndex >= words[currWordIndex].length) {
+      return 'has-background-danger'
+    } else {
+      return ''
+    }
+  }
+
+ const showFile = () => {
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+      var preview = document.getElementById('show-text');
+      var file = document.querySelector('input[type=file]').files[0];
+      var reader = new FileReader();
+
+      var textFile = /text.*/;
+
+      if (file.type.match(textFile)) {
+        reader.onload = function (event) {
+          let res = event.target.result;
+          console.log(res);
+          setWords(res.split(" "))
+        };
+      } else {
+        preview.innerHTML =
+          "<span class='error'>It doesn't seem to be a text file!</span>";
+      }
+      reader.readAsText(file);
+    } else {
+      alert('Your browser is too old to support HTML5 File API');
+    }
+  };
+
+  const onSecondsChange = (event)=>{
+    setSeconds(event.target.value);
+  }
+
+  const setTime = ()=>{
+    setCountDown(seconds);
+  }
+
+  return (
+    <div className="App">
+      <div className="mx-4 mt-4 d-flex flex-row justify-content-between">
+        <div className="border border-2 ">
+          
+          <h1>{countDown}</h1>
+        </div>
+        <div className="d-flex flex-column">
+          <input value={seconds} onChange={onSecondsChange}></input>
+          <button className='btn btn-primary' onClick={setTime}>Set Time</button>
+         </div>
+        <div className="">
+        <button className="btn btn-primary " onClick={start}>
+          Start
+        </button>
+      </div>
+        <div className="border border-2">
+          <input type="file" onChange={showFile}/>
+        </div>
+      </div>
+      <div className="control is-expanded m-4">
+        <input ref={textInput} disabled={status !== "started"} type="text" className="input" onKeyDown={handleKeyDown} value={currInput} onChange={(e) => setCurrInput(e.target.value)}  />
+      </div>
+      
+      {status === 'started' && (
+        <div className="m-4" >
+          <div className="card">
+            <div className="card-content">
+              <div className="content">
+                {words.map((word, i) => (
+                  <span key={i}>
+                    <span>
+                      {word.split("").map((char, idx) => (
+                        <span className={getCharClass(i, idx, char)} key={idx}>{char}</span>
+                      )) }
+                    </span>
+                    <span> </span>
+                  </span>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      {status === 'finished' && (
+        <div className="section">
+          <div className="columns">
+            <div className="column has-text-centered">
+              <p className="is-size-5">Words per minute:</p>
+              <p className="has-text-primary is-size-1">
+                {correct}
+              </p>
+            </div>
+            <div className="column has-text-centered">
+              <p className="is-size-5">Accuracy:</p>
+              {correct !== 0 ? (
+                <p className="has-text-info is-size-1">
+                  {Math.round((correct / (correct + incorrect)) * 100)}%
+                </p>
+              ) : (
+                <p className="has-text-info is-size-1">0%</p>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+
+    </div>
+  );
+}
+
+export default App;
